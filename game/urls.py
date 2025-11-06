d5e59b2e66aa8fa731a2a1c616a5d6e3d0573948
Riley Isom <rileyisom99@gmail.com>
2025-11-06 13:56:54 -0700

    initial commit

---
+++ game/urls.py
@@ -0,0 +1,13 @@
+# game/urls.py
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.level_list, name='level-list'),
+    path('level/<int:level_id>/', views.grid_view, name='grid'),
+    path("update_vehicle_position/<int:vehicle_id>/", views.update_vehicle_position, name="update_vehicle_position"),
+    path("mark_game_started/<int:level_id>/", views.mark_game_started, name="mark_game_started"),
+    path('reset_level/<int:level_id>/', views.reset_level, name='reset_level'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
