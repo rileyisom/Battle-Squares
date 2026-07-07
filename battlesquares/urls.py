eda4c697556f2c797663a5abc1ae4354fab541a5
Riley Isom <rileyisom99@gmail.com>
2026-07-06 18:14:14 -0600

    Rename gridgame project to battlesquares
    Renames the Django settings package and updates every internal reference
    (manage.py, ROOT_URLCONF, WSGI_APPLICATION, package-lock.json) to match
    the game's actual name and the GitHub repo (Battle-Squares).

--- battlesquares/urls.py
+++ battlesquares/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for gridgame project.
+URL configuration for battlesquares project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# gridgame/urls.py
+# battlesquares/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gridgame.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "battlesquares.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
